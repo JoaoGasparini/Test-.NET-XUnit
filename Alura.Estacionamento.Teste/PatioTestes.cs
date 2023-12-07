5df07307845d20496e9733aaf81fd5e9a2702062
João Gasparini <joao@mokainvest.com.br>
2023-12-07 16:59:03 -0300

    Automação de testes.

---
+++ Alura.Estacionamento.Teste/PatioTestes.cs
@@ -0,0 +1,57 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Teste
+{
+    public class PatioTestes
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //arrange
+            Patio patio = new();
+            Veiculo veiculo = new()
+            {
+                Proprietario = "João",
+                //Tipo = TipoVeiculo.Automovel,
+                Cor = "Verde",
+                Modelo = "Fusca",
+                Placa = "ASD-9999"
+            };
+
+            patio.RegistrarEntradaVeiculo(veiculo);
+            patio.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //act
+            double faturamento = patio.TotalFaturado();
+
+            //assert
+            Assert.Equal(2, faturamento);
+        }
+        [Theory]
+        [InlineData("André Silva", "ASD-9999", "PRETO", "GOL")]
+        [InlineData("ROBERTO Silva", "DNS-8741", "CINZA", "FUSCA")]
+        [InlineData("ANTONIO Silva", "GDR-1234", "AZUL", "OPALA")]
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            //ARRANGE
+            Patio Estacionamento = new();
+            var veiculo = new Veiculo();
+
+            veiculo.Proprietario = proprietario;
+            veiculo.Placa = placa;
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+
+            Estacionamento.RegistrarEntradaVeiculo(veiculo);
+            Estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //ACT
+            double faturamento = Estacionamento.TotalFaturado();
+
+            //ASSERT
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
---
+++ Alura.Estacionamento.Teste/VeiculoTestes.cs
@@ -0,0 +1,66 @@
+﻿using Alura.Estacionamento.Modelos;
+using Xunit;
+
+namespace Alura.Estacionamento.Teste
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        [Trait("Funcionalidade", "Acelerar")]
+        public void TestaVeiculoAcelerar()
+        {
+            //arrange
+            Veiculo veiculo = new();
+            //act
+            veiculo.Acelerar(10);
+            //assert
+            Assert.Equal(100, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        [Trait("Funcionalidade", "Frear")]
+        public void TestaVeiculoFrear()
+        {
+            //arrange
+            Veiculo veiculo = new();
+            //act
+            veiculo.Frear(10);
+            //assert
+            Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact(Skip = "Teste em desenvolvimento.")]
+        public void ComoIgnorarTeste()
+        {
+            //MANEIRA DE IGNORAR O TESTE
+        }
+
+        [Fact(DisplayName = "Terceiro teste")]
+        public void DisplayNameTeste()
+        {
+            //MANEIRA DE ADICIONAR NOME DO TESTE PERSONALIZADO EM VEZ DO METODO
+        }
+
+        [Fact(DisplayName = "Terceiro teste", Skip = "Pulando teste")]
+        public void DisplayNameTestePulaTeste()
+        {
+            //MANEIRA DE ADICIONAR NOME DO TESTE PERSONALIZADO EM VEZ DO METODO E PULA TESTE
+        }
+
+        [Theory]
+        [ClassData(typeof(Veiculo))]
+        public void TestaVeiculoClass(Veiculo modelo)
+        {
+            //Arrange 
+            var veiculo = new Veiculo();
+
+            //Act 
+            veiculo.Acelerar(10);
+            modelo.Acelerar(10);
+
+            //Assert 
+            Assert.Equal(modelo.VelocidadeAtual, veiculo.VelocidadeAtual);
+        }
+
+    }
+}
